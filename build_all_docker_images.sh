a8a1a75d6977997e4ed47c648520795155d00ee5
Alexandre Girard <git@alexgirard.com>
2020-04-21 15:08:26 +0200

    fix: only build deepdetect_cpu and deepdetect_gpu

--- build_all_docker_images.sh
+++ build_all_docker_images.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Set deepdetect version tags
-TAG_MODELS="v0.9.x-pre"
-TAG_NOMODELS="v0.9.x-nomodels-pre"
+DOCKER_TAG="v0.9.x-pre"
 
 # Set to true if you want to push images to dockerhub
 PUSH_DOCKERHUB=false
@@ -16,189 +15,37 @@ mkdir docker_build_logs
 
 echo 'Default cpu build'
 date
-docker build -t jolibrain/deepdetect_cpu:$TAG_MODELS \
+docker build -t jolibrain/deepdetect_cpu:$DOCKER_TAG \
+  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
   --no-cache \
   -f cpu.Dockerfile \
   . > docker_build_logs/deepdetect_cpu.log
 
-docker build -t jolibrain/deepdetect_cpu:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f cpu.Dockerfile \
-  . > docker_build_logs/deepdetect_cpu_nomodels.log
-
 if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_cpu:$TAG_MODELS
-  docker push jolibrain/deepdetect_cpu:$TAG_NOMODELS
-fi
-
-echo 'cpu_tf build'
-date
-docker build -t jolibrain/deepdetect_cpu_tf:$TAG_MODELS \
-  --build-arg DEEPDETECT_BUILD=caffe-tf \
-  --no-cache \
-  -f cpu.Dockerfile \
-  . > docker_build_logs/deepdetect_cpu_tf.log
-
-docker build -t jolibrain/deepdetect_cpu_tf:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_BUILD=caffe-tf \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f cpu.Dockerfile \
-  . > docker_build_logs/deepdetect_cpu_tf_nomodels.log
-
-if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_cpu_tf:$TAG_MODELS
-  docker push jolibrain/deepdetect_cpu_tf:$TAG_NOMODELS
+  docker push jolibrain/deepdetect_cpu:$DOCKER_TAG
 fi
 
 #
-# GPU - CUDA 9
+# GPU
 #
 
 echo 'Default gpu build'
 date
-docker build -t jolibrain/deepdetect_gpu:$TAG_MODELS \
+docker build -t jolibrain/deepdetect_gpu:$DOCKER_TAG \
+  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
   --no-cache \
   -f gpu.Dockerfile \
   . > docker_build_logs/deepdetect_gpu.log
 
-docker build -t jolibrain/deepdetect_gpu:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_nomodels.log
-
 if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_gpu:$TAG_MODELS
-  docker push jolibrain/deepdetect_gpu:$TAG_NOMODELS
-fi
-
-echo 'gpu_tf build'
-date
-docker build -t jolibrain/deepdetect_gpu_tf:$TAG_MODELS \
-  --build-arg DEEPDETECT_BUILD=caffe-tf \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_tf.log
-
-docker build -t jolibrain/deepdetect_gpu_tf:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_BUILD=caffe-tf \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_tf_nomodels.log
-
-if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_gpu_tf:$TAG_MODELS
-  docker push jolibrain/deepdetect_gpu_tf:$TAG_NOMODELS
-fi
-
-echo 'gpu_tf_cpu build'
-date
-docker build -t jolibrain/deepdetect_gpu_tf_cpu:$TAG_MODELS \
-  --build-arg DEEPDETECT_BUILD=caffe-cpu-tf \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_tf_cpu.log
-
-docker build -t jolibrain/deepdetect_gpu_tf_cpu:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_BUILD=caffe-cpu-tf \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_tf_cpu_nomodels.log
-
-if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_gpu_tf_cpu:$TAG_MODELS
-  docker push jolibrain/deepdetect_gpu_tf_cpu:$TAG_NOMODELS
-fi
-
-echo 'p100 gpu build'
-date
-docker build -t jolibrain/deepdetect_gpu_p100:$TAG_MODELS \
-  --build-arg DEEPDETECT_BUILD=p100 \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_p100.log
-
-docker build -t jolibrain/deepdetect_gpu_p100:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_BUILD=p100 \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_p100_nomodels.log
-
-if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_gpu_p100:$TAG_MODELS
-  docker push jolibrain/deepdetect_gpu_p100:$TAG_NOMODELS
-fi
-
-echo 'volta gpu build'
-date
-docker build -t jolibrain/deepdetect_gpu_volta:$TAG_MODELS \
-  --build-arg DEEPDETECT_BUILD=volta \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_volta.log
-
-docker build -t jolibrain/deepdetect_gpu_volta:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_BUILD=volta \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_volta_nomodels.log
-
-if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_gpu_volta:$TAG_MODELS
-  docker push jolibrain/deepdetect_gpu_volta:$TAG_NOMODELS
-fi
-
-echo 'gpu_faiss build'
-date
-docker build -t jolibrain/deepdetect_gpu_faiss:$TAG_MODELS \
-  --build-arg DEEPDETECT_BUILD=faiss \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_faiss.log
-
-docker build -t jolibrain/deepdetect_gpu_faiss:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_BUILD=faiss \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_faiss_nomodels.log
-
-if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_gpu_faiss:$TAG_MODELS
-  docker push jolibrain/deepdetect_gpu_faiss:$TAG_NOMODELS
-fi
-
-echo 'gpu_volta_faiss build'
-date
-docker build -t jolibrain/deepdetect_gpu_volta_faiss:$TAG_MODELS \
-  --build-arg DEEPDETECT_BUILD=volta-faiss \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_volta_faiss.log
-
-docker build -t jolibrain/deepdetect_gpu_volta_faiss:$TAG_NOMODELS \
-  --build-arg DEEPDETECT_BUILD=volta-faiss \
-  --build-arg DEEPDETECT_DEFAULT_MODELS=false \
-  --no-cache \
-  -f gpu.Dockerfile \
-  . > docker_build_logs/deepdetect_gpu_volta_faiss_nomodels.log
-
-if [ "$PUSH_DOCKERHUB" = true ] ; then
-  docker push jolibrain/deepdetect_gpu_volta_faiss:$TAG_MODELS
-  docker push jolibrain/deepdetect_gpu_volta_faiss:$TAG_NOMODELS
+  docker push jolibrain/deepdetect_gpu:$DOCKER_TAG
 fi
 
 ## TODO: build this one on armv7 hardware
 ##
 ## echo 'ncnn_pi3 build'
 ## date
-## docker build -t jolibrain/deepdetect_ncnn_pi3:$TAG_MODELS \
+## docker build -t jolibrain/deepdetect_ncnn_pi3:$DOCKER_TAG \
 ##   --build-arg DEEPDETECT_BUILD=armv7 \
 ##   --no-cache \
 ##   -f cpu-armv7.Dockerfile \
@@ -212,7 +59,7 @@ fi
 ##   . > docker_build_logs/deepdetect_ncnn_pi3_nomodels.log
 ##
 ## if [ "$PUSH_DOCKERHUB" = true ] ; then
-##   docker push jolibrain/deepdetect_ncnn_pi3:$TAG_MODELS
+##   docker push jolibrain/deepdetect_ncnn_pi3:$DOCKER_TAG
 ##   docker push jolibrain/deepdetect_ncnn_pi3:$TAG_NOMODELS
 ## fi
 ##
@@ -220,7 +67,7 @@ fi
 ##
 ## echo 'caffe2 gpu build'
 ## date
-## docker build -t jolibrain/deepdetect_gpu_caffe2:$TAG_MODELS \
+## docker build -t jolibrain/deepdetect_gpu_caffe2:$DOCKER_TAG \
 ##   --build-arg DEEPDETECT_BUILD=caffe2 \
 ##   --no-cache \
 ##   -f gpu.Dockerfile \
@@ -234,14 +81,14 @@ fi
 ##   . > docker_build_logs/deepdetect_gpu_caffe2_nomodels.log
 ##
 ## if [ "$PUSH_DOCKERHUB" = true ] ; then
-##   docker push jolibrain/deepdetect_gpu_caffe2:$TAG_MODELS
+##   docker push jolibrain/deepdetect_gpu_caffe2:$DOCKER_TAG
 ##   docker push jolibrain/deepdetect_gpu_caffe2:$TAG_NOMODELS
 ## fi
 ## TODO: deploy these configurations
 ##
 ## echo 'gpu_xavier build'
 ## date
-## docker build -t jolibrain/deepdetect_gpu_xavier:$TAG_MODELS \
+## docker build -t jolibrain/deepdetect_gpu_xavier:$DOCKER_TAG \
 ##   --build-arg DEEPDETECT_BUILD=volta \
 ##   --no-cache \
 ##   -f gpu.Dockerfile \
@@ -255,7 +102,7 @@ fi
 ##   . > docker_build_logs/deepdetect_gpu_xavier_nomodels.log
 ##
 ## if [ "$PUSH_DOCKERHUB" = true ] ; then
-##   docker push jolibrain/deepdetect_gpu_xavier:$TAG_MODELS
+##   docker push jolibrain/deepdetect_gpu_xavier:$DOCKER_TAG
 ##   docker push jolibrain/deepdetect_gpu_xavier:$TAG_NOMODELS
 ## fi
 ##
